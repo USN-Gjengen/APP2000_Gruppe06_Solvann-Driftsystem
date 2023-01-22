41ce3517d44419c92572e5d699ed3b98e3036bae
AtleFBerg <90725592+AtleFBerg@users.noreply.github.com>
2023-01-22 14:37:38 +0100

    An easy database test against mongoDB.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 const port = 21613;
 app.use(cors());
@@ -13,3 +14,19 @@ app.listen(port, () => {
   console.log("Heisann");
   console.log(`Example app listening on port ${port}`);
 })
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://eksempler.no:37191/abc');
+  const kittySchema = new mongoose.Schema({
+    name: String
+  });
+  const Kitten = mongoose.model('Kitten', kittySchema);
+  const fluffy = new Kitten({ name: 'bernard' });
+  await fluffy.save();
+  
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+
