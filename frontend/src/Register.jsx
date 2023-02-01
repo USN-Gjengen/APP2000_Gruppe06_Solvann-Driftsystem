e32872488c0d489bc15589c264c034a8afe40ef3
Exouxas <exouxas@gmail.com>
2023-02-01 16:15:38 +0100

    Remove star to test CI/CD

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -19,9 +19,9 @@ export const Register = (props) => {
             <label htmlFor="email">Email</label>
             <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="email@address.com" id="email" name="email"/>
             <label htmlFor="password">Password</label>
-            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="**********" id="password" name="password"/>
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*********" id="password" name="password"/>
             <label htmlFor="confirm-password">Confirm password</label>
-            <input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type="password" placeholder="**********" id="confirm-password" name="confirm-password"/>
+            <input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type="password" placeholder="*********" id="confirm-password" name="confirm-password"/>
             <button type="submit">Register</button>
         </form>
         <button className='link-btn' onClick={() => props.onFormSwitch('login')}>Already have an account? login here.</button>
