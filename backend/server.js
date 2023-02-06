d990075d5c1c1d64088288e753441b44e4cd778d
Exouxas <exouxas@gmail.com>
2023-02-06 13:25:25 +0100

    Made contact with all API`s, also made safe login for group API.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const cors = require('cors');
-const mongoose = require('mongoose');
+
+import * as functions from './functions.js' ;
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
 const app = express();
 const port = 21613;
 app.use(cors());
@@ -15,15 +18,18 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
 
+
+
+
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://eksempler.no:37191/abc');
+  await mongoose.connect('mongodb://username:password@eksempler.no:37191/?authMechanism=DEFAULT');
   const kittySchema = new mongoose.Schema({
     name: String
   });
   const Kitten = mongoose.model('Kitten', kittySchema);
-  const fluffy = new Kitten({ name: 'bernard' });
+  const fluffy = new Kitten({ name: 'berit' });
   await fluffy.save();
   
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
