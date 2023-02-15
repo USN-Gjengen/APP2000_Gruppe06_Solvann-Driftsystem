66a5a6fa88dfbd54dacb2642d696999c06eac018
AtleFBerg <90725592+AtleFBerg@users.noreply.github.com>
2023-02-15 14:05:12 +0100

    Fixed logging for database exports, new functions

--- backend/functiontest.js
+++ backend/functiontest.js
@@ -5,6 +5,16 @@ const dbfunctions = require('./dbfunctions.js');
 //functions.getSolarValue();
 //functions.getWaterInflux();
 //functions.getPowerPrice();
+//functions.setTurbineStatus("8d360792-d4ab-42df-927d-11a8a1c83edc", 0);
+//functions.setTurbineStatus("f39090e5-22bf-406f-ab96-7cd8b89180aa", 0);
+//functions.setTurbineStatus("ebe242ea-baea-4241-a158-763c429fd495", 0);
+//functions.setTurbineStatus("6381c1d6-b347-4afb-9b16-ba17ed239a95", 0);
 //functions.getGroupState();
 //functions.getTurbineStatus();
-//functions.setTurbineStatus("6381c1d6-b347-4afb-9b16-ba17ed239a95", 1);
+//dbfunctions.logPowerPrice(5);
+//functions.setAllTurbinesOn();
+//functions.setAllTurbinesOff();
+
+
+
+
