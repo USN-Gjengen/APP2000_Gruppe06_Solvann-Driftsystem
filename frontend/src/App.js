159e97c209150776930302e92abc30ae754c2021
Christoffer Riis <Christoffer@Riis.cc>
2023-02-22 12:36:19 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import Login from "./pages/Login";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./pages/login";
+import { BrowserRouter, Route} from "react-router-dom";
 import React from "react";
 import Prototype from "./pages/Prototype";
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="Appen">
       <BrowserRouter>
-        <Route exact path="/Login" component={Login} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/" component={Prototype} />
       </BrowserRouter>
     </div>
