b85f00b7595e5040867c6fb1c462e5e0d439cdf8
Exouxas <exouxas@gmail.com>
2023-03-07 16:37:53 +0100

    Fix document format/indentation (again)

--- backend/dbfunctions.js
+++ backend/dbfunctions.js
@@ -13,30 +13,30 @@ try {
 }
 
 const connect = async () => {
-    console.log("Connecting to database...");
-    mongoose.set('strictQuery', true);
-    await mongoose.connect('mongodb://' + DB_USR + ':' + DB_PWD + '@eksempler.no:37191/?authMechanism=DEFAULT');
-    switch(mongoose.connection.readyState){
-        case 0:
-            console.log("Error! Database is disconnected");
-            break;
-        case 1:
-            console.log("Database successfully connected!");
-            break;
-		    case 2:
-			      if(process.env.NODE_ENV != "test"){
-				        console.log("Error! Database is still connecting");
-			      }
-			      break;
-		    case 3:
-			      if(process.env.NODE_ENV != "test"){
-				        console.log("Error! Database is trying to disconnect");
-			      }
-			      break;
-    }
+	console.log("Connecting to database...");
+	mongoose.set('strictQuery', true);
+	await mongoose.connect('mongodb://' + DB_USR + ':' + DB_PWD + '@eksempler.no:37191/?authMechanism=DEFAULT');
+	switch (mongoose.connection.readyState) {
+		case 0:
+			console.log("Error! Database is disconnected");
+			break;
+		case 1:
+			console.log("Database successfully connected!");
+			break;
+		case 2:
+			if (process.env.NODE_ENV != "test") {
+				console.log("Error! Database is still connecting");
+			}
+			break;
+		case 3:
+			if (process.env.NODE_ENV != "test") {
+				console.log("Error! Database is trying to disconnect");
+			}
+			break;
+	}
 
-    return mongoose;
-} 
+	return mongoose;
+}
 
 const powerSchema = new mongoose.Schema({
 	price: Number,
@@ -45,11 +45,11 @@ const powerSchema = new mongoose.Schema({
 const PowerPrice = mongoose.model('PowerPrice', powerSchema);
 
 const logPowerPrice = async () => {
-  await functions.getPowerPrice().then(price => {
-    console.log(price);
-    const power = new PowerPrice({ price: price, date: Date.now()});
-    power.save();
-  });
+	await functions.getPowerPrice().then(price => {
+		console.log(price);
+		const power = new PowerPrice({ price: price, date: Date.now() });
+		power.save();
+	});
 }
 
 const solarSchema = new mongoose.Schema({
@@ -59,11 +59,11 @@ const solarSchema = new mongoose.Schema({
 const SolarValue = mongoose.model('SolarValue', solarSchema);
 
 const logSolarValue = async () => {
-  await functions.getSolarValue().then(value => {
-    console.log(value);
-    const solar = new SolarValue({ value: value, date: Date.now()});
-    solar.save();
-  });
+	await functions.getSolarValue().then(value => {
+		console.log(value);
+		const solar = new SolarValue({ value: value, date: Date.now() });
+		solar.save();
+	});
 }
 
 const waterInfluxSchema = new mongoose.Schema({
@@ -73,11 +73,11 @@ const waterInfluxSchema = new mongoose.Schema({
 const WaterInflux = mongoose.model('WaterInflux', waterInfluxSchema);
 
 const logWaterInflux = async () => {
-  await functions.getWaterInflux().then(waterInflux => {
-    console.log(waterInflux);
-    const influx = new WaterInflux({ waterInflux: waterInflux, date: Date.now()});
-    influx.save();
-  });
+	await functions.getWaterInflux().then(waterInflux => {
+		console.log(waterInflux);
+		const influx = new WaterInflux({ waterInflux: waterInflux, date: Date.now() });
+		influx.save();
+	});
 }
 
 const groupStateSchema = new mongoose.Schema({
@@ -89,29 +89,29 @@ const groupStateSchema = new mongoose.Schema({
 const GroupState = mongoose.model('GroupState', groupStateSchema);
 
 const logGroupState = async () => {
-  await functions.getGroupState().then(gs => {
-    const state = new GroupState({ money: gs.money, date: Date.now(), waterLevel: gs.waterLevel, environmentCost: gs.environmentCost});
-    state.save();
-    });
-  //mongoose.connection.close();
+	await functions.getGroupState().then(gs => {
+		const state = new GroupState({ money: gs.money, date: Date.now(), waterLevel: gs.waterLevel, environmentCost: gs.environmentCost });
+		state.save();
+	});
+	//mongoose.connection.close();
 }
 
 /*const getAllGroupStates = async () => {
   const cursor = GroupState.find({  }).cursor();
   var rv = [];
   for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
-    rv.push(doc);
+	rv.push(doc);
   }
   return rv;
 }*/
 
 const getAll = async (document) => {
-  const cursor = document.find({  }).cursor();
-  var rv = [];
-  for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
-    rv.push(doc);
-  }
-  return rv;
+	const cursor = document.find({}).cursor();
+	var rv = [];
+	for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
+		rv.push(doc);
+	}
+	return rv;
 }
 
 exports.connect = connect;
