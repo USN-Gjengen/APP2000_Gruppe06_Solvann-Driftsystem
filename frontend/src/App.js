89f9d3a8836b962a7eb3d018c54ff3c679a2a3d6
Christoffer Riis <Christoffer@Riis.cc>
2023-04-09 01:00:46 +0200

    Added subcomponents to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import React from "react";
 import Prototype from "./pages/Prototype";
 import Trend from "./pages/Trend";
 import WaterInflux from "./components/WaterInflux";
+import Money from "./components/Money";
+import WaterLevel from "./components/WaterLevel";
+import EnvironmentCost from "./components/EnvironmentCost";
 
 function App() {
 
@@ -17,6 +20,10 @@ function App() {
         <Route exact path="/Prototype" element={< Prototype />} ></Route>
         <Route exact path="/Trend" element={< Trend />} ></Route>
         <Route exact path="/components/WaterInflux" element={< WaterInflux />} ></Route>
+        <Route exact path="/components/Money" element={< Money />} ></Route>
+        <Route exact path="/components/WaterLevel" element={< WaterLevel />} ></Route>
+        <Route exact path="/components/EnvironmentCost" element={< EnvironmentCost />} ></Route>
+
       </Routes>
     </div>
     </Router>
