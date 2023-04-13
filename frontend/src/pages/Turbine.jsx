5ec0e1f37efd7afbc96ba8a0ed8fd1a00c2d111f
Christoffer Riis <Christoffer@Riis.cc>
2023-04-14 01:58:17 +0200

    add function for spinning turbine

--- frontend/src/pages/Turbine.jsx
+++ frontend/src/pages/Turbine.jsx
@@ -1,5 +1,8 @@
 import React, {useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSpring, animated, config } from "react-spring";
+import turbineIMG from "../img/Turbine.png";
+import "../Turbine.css";
 
 const Turbine = () => {
     
@@ -7,7 +10,17 @@ const Turbine = () => {
         const [logout, setLogout] = React.useState(false);
         const [groupState, setGroupState] = React.useState({ group: [] });
         const [isTurbineOn, setIsTurbineOn] = React.useState(false);
-    
+        const [isSpinning, setIsSpinning] = useState(false);
+
+        const props = useSpring({
+            from: { transform: "rotate(0deg)" },
+            to: { transform: `rotate(${isSpinning ? 360 : 0}deg)` },
+            config: { duration: 1000 },
+            reset: true,
+            loop: isSpinning,
+        });
+
+
         React.useEffect(() => {
             if (!localStorage.getItem("auth")) navigate("/login");
         }, [navigate, logout]);
@@ -44,54 +57,76 @@ const Turbine = () => {
         const handleTurbineOn = (e) => {
             e.preventDefault();
             setIsTurbineOn(true);
+            setIsSpinning(true);
         };
 
         const handleTurbineOff = (e) => {
             e.preventDefault();
             setIsTurbineOn(false);
+            setIsSpinning(false);
         };
+
     
     return (
-    
-        <div className="dashboard">
-            <div className="Header">
-                <div className="nav">
-                    <div className="left">
-                        <li>
-                            <button onClick={logoutHandler} className="btn">Logout</button>
-                        </li>
-                    </div>
-                    <div className="center">
-                        <li>
-                            <h1 className="dashboard-title">Dashboard</h1>
-                        </li>
-                    </div>
-                    <div className="right">
-                        <li>
-                            <button className="btn">Profile</button>
-                        </li>
+     <div className="dashboard">
+                <div className="Header">
+                    <div className="nav">
+                        <div className="left">
+                            <li>
+                                <button onClick={logoutHandler} className="btn">Logout</button>
+                            </li>
+                        </div>
+                        <div className="center">
+                            <li>
+                                <h1 className="dashboard-title">Dashboard</h1>
+                            </li>
+                        </div>
+                        <div className="right">
+                            <li>
+                                <button className="btn">Profile</button>
+                            </li>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="button-container">
-                <button className="btn" onClick={handleTurbineOn}>
-                    <span>Turn On</span>
-                </button>
-                <button className="btn" onClick={handleTurbineOff}>
-                    <span>Turn Off</span>
-                </button>
-                <button className="btn" onClick={handleUpdate}>
-                    <span>Update</span>
-                </button>
+                <div className="button-container">
+                    <button className="btn" onClick={handleTurbineOn}>
+                        <span>Turn On</span>
+                    </button>
+                    <button className="btn" onClick={handleTurbineOff}>
+                        <span>Turn Off</span>
+                    </button>
+                    <button className="btn" onClick={handleUpdate}>
+                        <span>Update</span>
+                    </button>
 
-            </div>
-            <div>    
-                Turbin status:{" "}
+                </div>
+                <div>
+                    Turbin status:{" "}
                     <span style={{ color: isTurbineOn ? "green" : "red" }}>
-                                {isTurbineOn ? "Aktive" : "Passiv"}
+                        {isTurbineOn ? "Aktive" : "Passiv"}
                     </span>
+                </div>
+                <div className="turbines-container">
+                    <animated.img
+                        className="turbine"
+                        style={props}
+                        src={turbineIMG}
+                        alt="Turbine"
+                        />
+                    <animated.img
+                        className="turbine"
+                        style={props}
+                        src={turbineIMG}
+                        alt="Turbine"
+                        />
+                    <animated.img
+                        className="turbine"
+                        style={props}
+                        src={turbineIMG}
+                        alt="Turbine"
+                        />
+                </div>
             </div>
-        </div>
             
     );
 };
