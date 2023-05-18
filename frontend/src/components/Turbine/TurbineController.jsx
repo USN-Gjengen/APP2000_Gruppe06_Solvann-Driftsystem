b248fd4e794c2569e94456b73d341cf5f47ec36c
Exouxas <exouxas@gmail.com>
2023-05-18 18:52:55 +0200

    Make turbines ALMOST work

--- frontend/src/components/Turbine/TurbineController.jsx
+++ frontend/src/components/Turbine/TurbineController.jsx
@@ -5,76 +5,24 @@ import { useTurbineContext } from "./TurbineProvider";
 import "../../Turbine.css";
 
 const TurbineController = () => {
-
-    const [groupState, setGroupState] = React.useState({ group: [] });
-    const { isTurbineOn, setIsTurbineOn } = useTurbineContext();
-    const [isSpinning, setIsSpinning] = useState(
-        localStorage.getItem("isSpinning") === "true" ? true : false
-    );
+    const [isSpinning, setIsSpinning] = useState(false);
 
     const props = useSpring({
         from: { transform: "rotate(0deg)" },
         to: { transform: `rotate(${isSpinning ? 360 : 0}deg)` },
-        config: { duration: 1000 },
+        config: { duration: 10000 },
         reset: true,
         loop: isSpinning,
     });
 
-    const handleTurbineOn = (e) => {
-        e.preventDefault();
-        setIsTurbineOn(true);
-        setIsSpinning(true);
-        localStorage.setItem("isSpinning", true);
-    };
-
-    const handleTurbineOff = (e) => {
-        e.preventDefault();
-        setIsTurbineOn(false);
-        setIsSpinning(false);
-        localStorage.setItem("isSpinning", false);
-    };
-
     return (
         <div>
-
-            <div className="button-container">
-                <button className="btn" onClick={handleTurbineOn}>
-                    <span>Turn On</span>
-                </button>
-                <button className="btn" onClick={handleTurbineOff}>
-                    <span>Turn Off</span>
-                </button>
-
-            </div>
-            <div>
-                Turbin status:{" "}
-                <span style={{ color: isTurbineOn ? "green" : "red" }}>
-                    {isTurbineOn ? "Aktive" : "Passiv"}
-                </span>
-            </div>
-            <div className="turbines-container">
-                <animated.img
-                    className="turbine"
-                    style={props}
-                    src={turbineIMG}
-                    alt="Turbine"
-                />
-                <animated.img
-                    className="turbine"
-                    style={props}
-                    src={turbineIMG}
-                    alt="Turbine"
-                />
-                <animated.img
-                    className="turbine"
-                    style={props}
-                    src={turbineIMG}
-                    alt="Turbine"
-                />
-            </div>
-
-            <div>
-            </div>
+            <animated.img
+                className="turbine"
+                style={props}
+                src={turbineIMG}
+                alt="Turbine"
+            />
         </div>
     );
 };
--- frontend/src/pages/Turbine.jsx
+++ frontend/src/pages/Turbine.jsx
@@ -4,43 +4,100 @@ import TurbineController from "../components/Turbine/TurbineController";
 
 
 const Turbine = () => {
-  const navigate = useNavigate();
-  const [logout, setLogout] = React.useState(false);
+	const navigate = useNavigate();
+	const [logout, setLogout] = React.useState(false);
 
-  React.useEffect(() => {
-    if (!localStorage.getItem("auth")) navigate("/login");
-  }, [navigate, logout]);
+	React.useEffect(() => {
+		if (!localStorage.getItem("auth")) navigate("/login");
+	}, [navigate, logout]);
 
-  const logoutHandler = (e) => {
-    e.preventDefault();
-    localStorage.removeItem("auth");
-    setLogout(true);
-  };
+	const logoutHandler = (e) => {
+		e.preventDefault();
+		localStorage.removeItem("auth");
+		setLogout(true);
+	};
 
-  return (
-    <div className="dashboard">
-        <div className="Header">
-            <div className="nav">
-                <div className="left">
-                      <li>
-                          <button onClick={logoutHandler} className="btn">Logg ut</button>
-                      </li>
-                </div>
-                  <div className="center">
-                      <li>
-                          <h1 className="dashboard-title">Turbiner</h1>
-                      </li>
-                  </div>
-                  <div className="right">
-                      <li>
-                          <button className="btn">Profil</button>
-                      </li>
-              </div>
-            </div>
-        </div>
-       <TurbineController/>
-    </div>
-            
-    );
+	const [turbineList, setTurbineList] = useState([]);
+
+	const getTurbines = async () => {
+		//const response = await fetch("http://api.solvann.eksempler.no/api/turbines/all", {
+		const response = await fetch("http://localhost:21613/api/turbines/all", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			},
+		});
+
+		const data = await response.json();
+		return data;
+	}
+
+	const handleTurbineOn = (e) => {
+		e.preventDefault();
+
+		// Set turbine status and if it's spinning
+	};
+
+	const handleTurbineOff = (e) => {
+		e.preventDefault();
+
+		// Set turbine status and if it's spinning
+	};
+
+
+	React.useEffect(() => {
+		setInterval(async () => {
+			let turbines = await getTurbines();
+			setTurbineList(turbines.map((turbine) => {
+
+				return (
+					<TurbineController 
+					key={turbine.id} 
+					id={turbine.id} 
+					isSpinning={turbine.capacityUsage != 0}
+					/>
+				)
+			}));
+		}, 4000);
+	}, []);
+
+	return (
+		<div className="dashboard">
+			<div className="Header">
+				<div className="nav">
+					<div className="left">
+						<li>
+							<button onClick={logoutHandler} className="btn">Logg ut</button>
+						</li>
+					</div>
+					<div className="center">
+						<li>
+							<h1 className="dashboard-title">Turbiner</h1>
+						</li>
+					</div>
+					<div className="right">
+						<li>
+							<button className="btn">Profil</button>
+						</li>
+					</div>
+				</div>
+			</div>
+
+
+			<div className="button-container">
+				<button className="btn" onClick={handleTurbineOn}>
+					<span>Turn On</span>
+				</button>
+				<button className="btn" onClick={handleTurbineOff}>
+					<span>Turn Off</span>
+				</button>
+			</div>
+
+			<div className="turbines-container">
+				{turbineList}
+			</div>
+		</div>
+
+	);
 };
 export default Turbine;
