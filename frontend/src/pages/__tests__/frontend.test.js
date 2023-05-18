9e81b06099e1e45a6a7374d619b242d6f789e0ed
Christoffer Riis <Christoffer@Riis.cc>
2023-05-18 11:59:19 +0200

    Run test

--- frontend/src/pages/__tests__/frontend.test.js
+++ frontend/src/pages/__tests__/frontend.test.js
@@ -3,10 +3,21 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Prototype from "../Prototype";
 import { BrowserRouter } from "react-router-dom";
+import { Chart } from "react-chartjs-2";
+
 
 const renderWithRouter = (component) => {
   return render(<BrowserRouter>{component}</BrowserRouter>);
 };
+
+describe("Prototype component", () => {
+  beforeAll(() => {
+    // Mock the Chart component
+    Chart.mockImplementation(() => ({
+      // Implement any necessary methods or properties here
+      // You can return empty implementations or mock data as needed
+    }));
+  });
 
 describe("Prototype component", () => {
   test("renders without crashing", () => {
@@ -17,6 +28,7 @@ describe("Prototype component", () => {
   test("displays the correct initial state", () => {
     renderWithRouter(<Prototype />);
 
-    expect(screen.getByText("Dashboard")).toBeInTheDocument();
+    expect(screen.getByText("Solvann Driftsystem")).toBeInTheDocument();
   });
+});
 });
