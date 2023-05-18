6996a51d7169a3d7a125e14641b71730dce6923f
Christoffer Riis <Christoffer@Riis.cc>
2023-05-18 12:08:43 +0200

    Run test again!

--- frontend/src/pages/__tests__/frontend.test.js
+++ frontend/src/pages/__tests__/frontend.test.js
@@ -3,21 +3,18 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Prototype from "../Prototype";
 import { BrowserRouter } from "react-router-dom";
-import { Chart } from "react-chartjs-2";
 
 
 const renderWithRouter = (component) => {
   return render(<BrowserRouter>{component}</BrowserRouter>);
 };
 
-describe("Prototype component", () => {
-  beforeAll(() => {
-    // Mock the Chart component
-    Chart.mockImplementation(() => ({
-      // Implement any necessary methods or properties here
-      // You can return empty implementations or mock data as needed
-    }));
-  });
+jest.mock("react-chartjs-2", () => ({
+  // Mocked Chart component
+  Line: () => null,
+  Bar: () => null,
+  // Add other chart types here if needed
+}));
 
 describe("Prototype component", () => {
   test("renders without crashing", () => {
@@ -30,5 +27,4 @@ describe("Prototype component", () => {
 
     expect(screen.getByText("Solvann Driftsystem")).toBeInTheDocument();
   });
-});
 });
