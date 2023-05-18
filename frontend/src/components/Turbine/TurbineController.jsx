c68f8670bcaff16d87811dfc46cd4c4ff4c02fac
Exouxas <exouxas@gmail.com>
2023-05-18 17:07:04 +0200

    Remove update button

--- frontend/src/components/Turbine/TurbineController.jsx
+++ frontend/src/components/Turbine/TurbineController.jsx
@@ -5,12 +5,12 @@ import { useTurbineContext } from "./TurbineProvider";
 import "../../Turbine.css";
 
 const TurbineController = () => {
-    
+
     const [groupState, setGroupState] = React.useState({ group: [] });
     const { isTurbineOn, setIsTurbineOn } = useTurbineContext();
     const [isSpinning, setIsSpinning] = useState(
         localStorage.getItem("isSpinning") === "true" ? true : false
-      );
+    );
 
     const props = useSpring({
         from: { transform: "rotate(0deg)" },
@@ -26,7 +26,7 @@ const TurbineController = () => {
         setIsSpinning(true);
         localStorage.setItem("isSpinning", true);
     };
-    
+
     const handleTurbineOff = (e) => {
         e.preventDefault();
         setIsTurbineOn(false);
@@ -34,63 +34,46 @@ const TurbineController = () => {
         localStorage.setItem("isSpinning", false);
     };
 
-    const handleUpdate = async () => {
-        var response = await fetch(
-          "http://api.solvann.eksempler.no/api/groupstates/last",
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        var data = await response.json();
-        setGroupState(data);
-      };
-
     return (
         <div>
-            
-                    <div className="button-container">
-                        <button className="btn" onClick={handleTurbineOn}>
-                            <span>Turn On</span>
-                        </button>
-                        <button className="btn" onClick={handleTurbineOff}>
-                            <span>Turn Off</span>
-                        </button>
-                        <button className="btn" onClick={handleUpdate}>
-                            <span>Update</span>
-                        </button>
 
-                    </div>
-                    <div>
-                        Turbin status:{" "}
-                        <span style={{ color: isTurbineOn ? "green" : "red" }}>
-                            {isTurbineOn ? "Aktive" : "Passiv"}
-                        </span>
-                    </div>
-                    <div className="turbines-container">
-                        <animated.img
-                            className="turbine"
-                            style={props}
-                            src={turbineIMG}
-                            alt="Turbine"
-                            />
-                        <animated.img
-                            className="turbine"
-                            style={props}
-                            src={turbineIMG}
-                            alt="Turbine"
-                            />
-                        <animated.img
-                            className="turbine"
-                            style={props}
-                            src={turbineIMG}
-                            alt="Turbine"
-                            />
-                    </div>
+            <div className="button-container">
+                <button className="btn" onClick={handleTurbineOn}>
+                    <span>Turn On</span>
+                </button>
+                <button className="btn" onClick={handleTurbineOff}>
+                    <span>Turn Off</span>
+                </button>
 
-                    <div>
+            </div>
+            <div>
+                Turbin status:{" "}
+                <span style={{ color: isTurbineOn ? "green" : "red" }}>
+                    {isTurbineOn ? "Aktive" : "Passiv"}
+                </span>
+            </div>
+            <div className="turbines-container">
+                <animated.img
+                    className="turbine"
+                    style={props}
+                    src={turbineIMG}
+                    alt="Turbine"
+                />
+                <animated.img
+                    className="turbine"
+                    style={props}
+                    src={turbineIMG}
+                    alt="Turbine"
+                />
+                <animated.img
+                    className="turbine"
+                    style={props}
+                    src={turbineIMG}
+                    alt="Turbine"
+                />
+            </div>
+
+            <div>
             </div>
         </div>
     );
