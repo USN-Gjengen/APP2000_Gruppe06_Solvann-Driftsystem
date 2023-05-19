fb48ce1cfab3cd3c1673d2fe970b04ee18002379
Exouxas <exouxas@gmail.com>
2023-05-19 11:52:26 +0200

    Use properties to set animation

--- frontend/src/components/Turbine/TurbineController.jsx
+++ frontend/src/components/Turbine/TurbineController.jsx
@@ -4,22 +4,21 @@ import { useSpring, animated, config } from "react-spring";
 import { useTurbineContext } from "./TurbineProvider";
 import "../../Turbine.css";
 
-const TurbineController = () => {
-    const [isSpinning, setIsSpinning] = useState(false);
-
-    const props = useSpring({
+const TurbineController = (props) => {
+    const style = useSpring({
         from: { transform: "rotate(0deg)" },
-        to: { transform: `rotate(${isSpinning ? 360 : 0}deg)` },
-        config: { duration: 10000 },
+        to: { transform: `rotate(${props.isSpinning ? 360 : 0}deg)` },
+        config: { duration: 1000 },
         reset: true,
-        loop: isSpinning,
+        loop: props.isSpinning,
     });
 
+    
     return (
         <div>
             <animated.img
                 className="turbine"
-                style={props}
+                style={style}
                 src={turbineIMG}
                 alt="Turbine"
             />
