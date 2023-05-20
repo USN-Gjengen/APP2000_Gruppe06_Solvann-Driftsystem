0c571d89a34f5ec7b5045f16da78c4f04b63db4e
Christoffer Riis <Christoffer@Riis.cc>
2023-05-20 16:32:03 +0200

    resolve conlict app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import Prototype from "./pages/Prototype";
 import Trend from "./pages/Trend";
 import Turbine from "./pages/Turbine";
 import Logg from "./pages/Logg";
-import Waterlevel from "./pages/Waterlevel";
 import WaterInflux from "./components/Graphs/WaterInflux";
 import WaterInfluxDash from "./components/Graphs/WaterInfluxDash";
 import Money from "./components/Graphs/Money";
@@ -17,28 +16,27 @@ import TurbineController from "./components/Turbine/TurbineController";
 import WaterContainer from "./components/Waterlevel/WaterContainer";
 
 function App() {
-
   return (
     <TurbineProvider>
       <Router>
-      <div className="Appen">
-        <Routes>
-          <Route exact path="/" element={< Login />}></Route>
-          <Route exact path="/login" element={< Login />}></Route>
-          <Route exact path="/Prototype" element={< Prototype />} ></Route>
-          <Route exact path="/Trend" element={< Trend />} ></Route>
-          <Route exact path="/Turbine" element={< Turbine />} ></Route>
-          <Route exact path="/Logg" element={< Logg />} ></Route>
-          <Route exact path="/Waterlevel" element={< Waterlevel />} ></Route>
-          <Route exact path="/components/WaterInflux" element={< WaterInflux />} ></Route>
-          <Route exact path="/components/WaterInfluxDash" element={< WaterInfluxDash />} ></Route>
-          <Route exact path="/components/Money" element={< Money />} ></Route>
-          <Route exact path="/components/WaterLevelGraph" element={< WaterLevelGraph />} ></Route>
-          <Route exact path="/components/EnvironmentCost" element={< EnvironmentCost />} ></Route>
-          <Route exact path="/components/Turbine/TurbineController" element={< TurbineController />} ></Route>
-          <Route exact path="/components/Waterlevel/WaterContainer" element={< WaterContainer />} ></Route>
-        </Routes>
-      </div>
+        <div className="Appen">
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/Prototype" element={<Prototype />} />
+            <Route exact path="/Trend" element={<Trend />} />
+            <Route exact path="/Turbine" element={<Turbine />} />
+            <Route exact path="/Logg" element={<Logg />} />
+            <Route exact path="/Waterlevel" element={<WaterContainer />} />
+            <Route exact path="/components/WaterInflux" element={<WaterInflux />} />
+            <Route exact path="/components/WaterInfluxDash" element={<WaterInfluxDash />} />
+            <Route exact path="/components/Money" element={<Money />} />
+            <Route exact path="/components/WaterLevelGraph" element={<WaterLevelGraph />} />
+            <Route exact path="/components/EnvironmentCost" element={<EnvironmentCost />} />
+            <Route exact path="/components/Turbine/TurbineController" element={<TurbineController />} />
+            <Route exact path="/components/Waterlevel/WaterContainer" element={<WaterContainer />} />
+          </Routes>
+        </div>
       </Router>
     </TurbineProvider>
   );
