1ffed51bd63b269b528d00ecce6184340515f8ec
Christoffer Riis <Christoffer@Riis.cc>
2023-05-21 11:52:50 +0200

    xD

--- frontend/src/pages/Prototype.jsx
+++ frontend/src/pages/Prototype.jsx
@@ -1,43 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Line } from "react-chartjs-2";
 import WaterInfluxDash from "../components/Graphs/WaterInfluxDash";
 import { useTurbineContext } from "../components/Turbine/TurbineProvider";
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  LineElement,
-  Legend,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Filler,
-} from "chart.js";
-ChartJS.register(
-  Title,
-  Tooltip,
-  LineElement,
-  Legend,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Filler
-);
+import HeaderSection from "../components/HeaderSection";
+
 
 const Prototype = () => {
         const navigate = useNavigate();
-        const { isTurbineOn, setIsTurbineOn } = useTurbineContext();
-        const [logout, setLogout] = React.useState(false);
-        const [groupState, setGroupState] = React.useState({ group: [] });
+        const { isTurbineOn } = useTurbineContext();
         const [trend, setTrend] = React.useState(false);
         const [turbine, setTurbine] = React.useState(false);
         const [logg, setLogg] = React.useState(false);
-        const [waterlevel, setWaterlevel] = React.useState(false);
-
-        React.useEffect(() => {
-            if (!localStorage.getItem("auth")) navigate("/login");
-        }, [navigate, logout]);
 
         React.useEffect(() => {
             if (localStorage.getItem("auth") && trend) {
@@ -58,13 +31,7 @@ const Prototype = () => {
         }, [navigate, logg]);
 
         React.useEffect(() => {
-            if (localStorage.getItem("auth") && waterlevel) {
-            navigate("/Waterlevel");
-            }
-        }, [navigate, waterlevel]);
-
-        React.useEffect(() => {
-            fetch("http://api.solvann.eksempler.no/api/turbines/all", {
+            fetch("http://" + process.env.REACT_APP_FRONTEND_API_ADDRESS + "/api/turbines/all", {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
@@ -74,12 +41,6 @@ const Prototype = () => {
             console.error(error);
             });
         }, [isTurbineOn]);
-        
-        const logoutHandler = (e) => {
-            e.preventDefault();
-            localStorage.removeItem("auth");
-            setLogout(true);
-        };
 
         const handleTrend = (e) => {
             e.preventDefault();
@@ -96,51 +57,12 @@ const Prototype = () => {
             setLogg(true);
         };
 
-        const handleWaterlevel = (e) => {
-            e.preventDefault();
-            setWaterlevel(true);
-        };
-
-        const handleUpdate = async () => {
-            var response = await fetch(
-            "http://api.solvann.eksempler.no/api/groupstates/last",
-            {
-                method: "GET",
-                headers: {
-                "Content-Type": "application/json",
-                },
-            }
-            );
-            var data = await response.json();
-            setGroupState(data);
-        };
-
+  
     return (
         <div className="dashboard">
-            <div className="Header">
-                <div className="nav">
-                <div className="left">
-                    <li>
-                    <button onClick={logoutHandler} className="btn">
-                        Logg ut
-                    </button>
-                    </li>
-                </div>
-                <div className="center">
-                    <li>
-                    <h1 className="dashboard-title">Solvann Driftsystem</h1>
-                    </li>
-                </div>
-                <div className="right">
-                    <li>
-                    <button className="btn">Profil</button>
-                    </li>
-                </div>
-                </div>
-            </div>
-            
+        <HeaderSection></HeaderSection>
+        
                 <div className="wrapper">
-                    
                     <div className="container">
 
                         <div className="row">
@@ -165,9 +87,7 @@ const Prototype = () => {
                                 <div className="info">
                                 <div className="sub">Vannstandskontroll</div>
                                 <div className="title">Nivåindikator</div>
-                                    <button className="btn" onClick={handleWaterlevel}>
-                                        Åpne Vannstand
-                                    </button>
+                                    <button className="btn">Åpne Vannstand</button>
                                 </div>
                             </div>
 
