6a0909f90bc5e253556792327a9c52c96d45de9a
Christoffer Riis <Christoffer@Riis.cc>
2023-05-23 20:21:41 +0200

    Clean and align trends

--- frontend/src/pages/Trend.jsx
+++ frontend/src/pages/Trend.jsx
@@ -5,60 +5,48 @@ const Trend = () => {
     return (
 
         <div className="trend">
-            <div className='boxes'>
-                <div className='container2'>
-                    <button className="btn-box">
+            <div className='bokser'>
+                <div className='trend-Konteiner'>
                         <h2>Water Influx</h2>
                         <Graph
                             src={`http://${process.env.REACT_APP_FRONTEND_API_ADDRESS}/api/WaterInflux/lastHour`}
                             title="Water Influx"
                         />
-                    </button>
                 </div>
-                <div className='container2'>
-                    <button className="btn-box">
+                <div className='trend-Konteiner'>
                         <h2>Money</h2>
                         <Graph
                             src={`http://${process.env.REACT_APP_FRONTEND_API_ADDRESS}/api/groupstates/lastHourMoney`}
                             title="Money"
                         />
-                    </button>
                 </div>
-                <div className='container2'>
-                    <button className="btn-box">
+                <div className='trend-Konteiner'>
                         <h2>Water Level</h2>
                         <Graph
                             src={`http://${process.env.REACT_APP_FRONTEND_API_ADDRESS}/api/groupstates/lastHourWaterLevel`}
                             title="Water Level"
                         />
-                    </button>
                 </div>
-                <div className='container2'>
-                    <button className="btn-box">
+                <div className='trend-Konteiner'>
                         <h2>Environment Cost</h2>
                         <Graph
                             src={`http://${process.env.REACT_APP_FRONTEND_API_ADDRESS}/api/groupstates/lastHourEnvironmentCost`}
                             title="Environment Cost"
                         />
-                    </button>
                 </div>
-                <div className='container2'>
-                    <button className="btn-box">
+                <div className='trend-Konteiner'>
                         <h2>Solar Value</h2>
                         <Graph
                             src={`http://${process.env.REACT_APP_FRONTEND_API_ADDRESS}/api/solarvalue/lastHour`}
                             title="Solar Value"
                         />
-                    </button>
                 </div>
-                <div className='container2'>
-                    <button className="btn-box">
+                <div className='trend-Konteiner'>
                         <h2>Power Price</h2>
                         <Graph
                             src={`http://${process.env.REACT_APP_FRONTEND_API_ADDRESS}/api/powerprice/lastHour`}
                             title="Power Price"
                         />
-                    </button>
                 </div>
             </div>
         </div>
