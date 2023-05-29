92bc91f4cff253242b700108034c15e847af719c
Christoffer Riis <Christoffer@Riis.cc>
2023-05-29 14:01:04 +0200

    add waterinflux/powerprice/solarvalue to turbine

--- frontend/src/pages/Turbine.jsx
+++ frontend/src/pages/Turbine.jsx
@@ -9,6 +9,9 @@ import "../styles/App.css";
 const Turbine = () => {
 
 	const [turbineList, setTurbineList] = useState([]);
+	const [waterInflux, setWaterInflux] = useState(0);
+	const [powerPrice, setPowerPrice] = useState(0);
+	const [solar, setSolar] = useState(0);
 
 	const getTurbines = async () => {
 		const response = await fetch("http://" + process.env.REACT_APP_FRONTEND_API_ADDRESS + "/api/turbines/all", {
@@ -33,6 +36,79 @@ const Turbine = () => {
 			})
 		});
 	}
+
+	const fetchWaterInflux = async () => {
+		try {
+			const response = await fetch(
+				"http://" + process.env.REACT_APP_FRONTEND_API_ADDRESS + "/api/waterInflux/last"
+			);
+			const jsonData = await response.json();
+
+			if (typeof jsonData === 'object' && jsonData !== null) {
+				const waterInflux = jsonData.value;
+				setWaterInflux(waterInflux);
+			} else {
+				console.error('Error: jsonData is not an object', jsonData);
+			}
+
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	const fetchPowerPrice = async () => {
+		try {
+			const response = await fetch(
+				"http://" + process.env.REACT_APP_FRONTEND_API_ADDRESS + "/api/powerprice/last"
+			);
+			const jsonData = await response.json();
+			
+			if (typeof jsonData === 'object' && jsonData !== null) {
+				const powerPrice = jsonData.value;
+				setPowerPrice(powerPrice);
+			} else {
+				console.error('Error: jsonData is not an object', jsonData);
+			}
+
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	const fetchSolar = async () => {
+		try {
+			const response = await fetch(
+				"http://" + process.env.REACT_APP_FRONTEND_API_ADDRESS + "/api/solarvalue/last"
+			);
+			const jsonData = await response.json();
+
+			if (typeof jsonData === 'object' && jsonData !== null) {
+				const solar = jsonData.value;
+				setSolar(solar);
+			} else {
+				console.error('Error: jsonData is not an object', jsonData);
+			}
+
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	useEffect(() => {
+		fetchWaterInflux();
+		fetchPowerPrice();
+		fetchSolar();
+		const interval = setInterval(() => {
+			fetchWaterInflux();
+			fetchPowerPrice();
+			fetchSolar();
+		}, 30000);
+
+		return () => clearInterval(interval);
+		
+	}, []);
+	
+
 
 	const handleTurbineForward = (e) => {
 		e.preventDefault();
@@ -86,16 +162,43 @@ const Turbine = () => {
 	}, []);
 
 	return (
+		
 		<div className="turbinePage">
+			<div className="info-container">
+				<div className="waterInflux-info">
+					<span className="value-info">
+						{waterInflux.toFixed(2)} m3/s
+					</span>
+					<div>
+						Vanntilstrømming
+					</div>
+				</div>
+				<div className="powerPrice-info">
+					<span className="value-info">
+						{powerPrice.toFixed(2)} NOK/MWh
+					</span>
+					<div>
+						Strømpris
+					</div>
+				</div>
+				<div className="solar-info">
+					<span className="value-info">
+						{solar.toFixed(2)} MWh
+					</span>
+					<div>
+					 	Solenergiverdi
+					</div>
+				</div>
+			</div>
 			<div className="button-container">
 				<button className="btn" onClick={handleTurbineForward} style={{background:"linear-gradient(90deg, #00ff00 0%, transparent 45%, transparent 55%, #00ff00 100%)",}}>
-					<span>Max Forward</span>
+					<span>Maks Forover</span>
 				</button>
 				<button className="btn" onClick={handleTurbineOff}  style={{background:"linear-gradient(90deg, #ff0d00 0%, transparent 45%, transparent 55%, #ff0d00	 100%)",}}>
-					<span>Turn Off</span>
+					<span>Skru av</span>
 				</button>
 				<button className="btn" onClick={handleTurbineReverse} style={{background:"linear-gradient(50deg, #faa49d 20%, transparent 45%, transparent 55%, #faa49d 100%)",}}>
-					<span>Max Reverse</span>
+					<span>Maks revers</span>
 				</button>
 			</div>
 
